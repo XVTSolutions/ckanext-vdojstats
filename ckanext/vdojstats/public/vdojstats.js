22b23f23772f8b12141fe045004915fca1259f90
mitsuhiro <mitsuhiro@mitsuhiro-Virtual-Machine.(none)>
2014-03-26 03:26:42 +1100

    CVJ-93

--- ckanext/vdojstats/public/vdojstats.js
+++ ckanext/vdojstats/public/vdojstats.js
@@ -1,33 +1,46 @@
+ckan.module('search_option_popover', function ($, _) {
+  return {
+    initialize: function () {
+      this.el.on('click', this._onClick);    
+    },
 
-  $(document).ready(function(){ 
-/*
+    _onClick: function(event) {
+
+        if($(this).hasClass('icon-minus-sign')){
+            $('#search_option_panel').hide(500);
+            $(this).removeClass('icon-minus-sign').addClass('icon-plus-sign');
+        }else{
+            $('#search_option_panel').show(500);
+            $(this).removeClass('icon-plus-sign').addClass('icon-minus-sign');
+        }
+    }
+  };
+});
+
+ckan.module('generatePDF', function ($, _) {
+  return {
+
+    initialize: function () {
+        this.el.on('click', this._onClick);    
+    },
+
+    _onClick: function(event) {
         var specialElementHandlers = {
             '#editor': function (element,renderer) {
                 return true;
             }
         };
-        $('#generatePDF').click(function () {
-            var doc = new jsPDF();
-            doc.fromHTML($('#target').html(), 15, 15, {
-                'width': 170,'elementHandlers': specialElementHandlers
-            });
-            doc.save('sample-file.pdf');
+
+        var doc = new jsPDF();
+        doc.fromHTML($('#content h2').html(), 15, 15, {
+            'width': 170,'elementHandlers': specialElementHandlers
         });
-    var doc = new jsPDF();
-    doc.text(20, 20, 'Hello world.');
-    doc.save('Test.pdf');
-*/
+          var string = doc.output('datauristring');
+          var x = window.open();
+          x.document.open();
+          x.document.location=string;
+    }
+  };
+});
 
-        $('#search_option_trigger').on('click', function(event){
-            if($(this).hasClass('icon-minus-sign')){
-                $('#search_option_panel').hide(500);
-                $(this).removeClass('icon-minus-sign').addClass('icon-plus-sign');
-            }else{
-                $('#search_option_panel').show(500);
-                $(this).removeClass('icon-plus-sign').addClass('icon-minus-sign');
-            }
-        });
-
-
-    });
 
